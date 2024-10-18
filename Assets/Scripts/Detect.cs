e33bb4632eb533066a8ffe96ac8b506eb9890da9
Antoine Yon <antoine.yon01@gmail.com>
2024-10-18 17:46:15 +0200

    up
    truc wtf avec le clamp

--- Assets/Scripts/Detect.cs
+++ Assets/Scripts/Detect.cs
@@ -6,6 +6,7 @@ public class Detect : MonoBehaviour
 {
 
     [SerializeField] private Transform Target;
+    [SerializeField] private Transform DebugObj;
 
     private Transform Boundaries;
     private Transform TargTop;
@@ -38,8 +39,8 @@ public class Detect : MonoBehaviour
 
     private bool RayDetectTarget()
     {
-        Vector2 target = new Vector2(LimitDist.position.x, BoundRay(Target.position.y));
-        RaycastHit2D hit = Physics2D.Raycast(POV.position, target, Vector3.Distance(Target.position, POV.position));
+        Vector2 target = new Vector2(Detectfollow(Target.position.x), BoundRay(Target.position.y));
+        RaycastHit2D hit = Physics2D.Raycast(POV.position, target, Vector3.Distance(target, POV.position));
         Debug.DrawLine(POV.position, target, Color.red);
         if (hit && hit.transform.tag == "Player")
         {
@@ -49,10 +50,37 @@ public class Detect : MonoBehaviour
         return false;
     }
 
+
+    //(float ? float : float)
+
     private float BoundRay(float y)
     {
-        float MaxVis = Mathf.Clamp(y, BotLimit.position.y, TopLimit.position.y);
+        float MaxVis = 0;
+        if (y > POV.position.y && !(CheckBound(Target.position.x, TopLimit.position) > Target.position.y))
+        {
+            MaxVis = CheckBound(Target.position.x, TopLimit.position);
+        }
+        else if (y < POV.position.y && !(CheckBound(Target.position.x, BotLimit.position) < Target.position.y))
+        {
+            MaxVis = CheckBound(Target.position.x, BotLimit.position);
+        }
+        MaxVis = Mathf.Clamp(y, BotLimit.position.y, TopLimit.position.y);
+        // MaxVis /= Vector2.Distance(POV.position, Target.position); // Pour créer une véritable sensation de cône de détection
         return MaxVis;
+    }
+
+    private float CheckBound(float x, Vector3 Limit) // si erreur voire l'origine
+    {
+        //float y = (Limit.y - POV.position.y) / (Limit.x - POV.position.x) * (x - POV.position.x)  + POV.position.y;
+        Vector2 LimtRel = (Limit - POV.position);
+        float a = (LimtRel.y /LimtRel.x) * (x - POV.position.x) + POV.position.y;
+        DebugObj.position = new Vector2(x, a);
+        return a;
+    }
+    private float Detectfollow(float x)
+    {
+        float DistVis = Mathf.Clamp(x, POV.position.x, LimitDist.position.x);
+        return DistVis;
     }
 
     public bool IsDetected()
